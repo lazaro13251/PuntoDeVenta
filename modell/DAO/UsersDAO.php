3be1c23ef8d55ab37cbd3e362f8378f7802f063a
lazaro13251 <lazaro13251@gmail.com>
2023-12-26 21:41:29 -0600

    Se cargan las modificaciones de la vista principal de usuario y se carga la estructura de la base de datos

--- modell/DAO/UsersDAO.php
+++ modell/DAO/UsersDAO.php
@@ -20,7 +20,31 @@ class UsersDAO implements IMethods {
     }
 
     public function ReadAll(): array {
-        
+        try {
+            $columns = $this->table->getCamposTabla();
+            $stmt = DBConn::obtenerConexion()->prepare("SELECT * FROM {$this->table->getNombreTabla()} WHERE $columns[4] = 1 ORDER BY $columns[0] DESC;");
+            $stmt->execute();
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $array = [];
+
+            foreach ($results as $result) :
+                $user = new Users(
+                        $result[$columns[0]],
+                        $result[$columns[1]],
+                        $result[$columns[2]],
+                        $result[$columns[3]],
+                        $result[$columns[4]],
+                        $result[$columns[5]],
+                        $result[$columns[6]]
+                );
+                $array[] = $user;
+            endforeach;
+            return $array;
+        } catch (Exception $exc) {
+            echo '<pre>';
+            var_dump($exc->getTraceAsString());
+            echo '</pre>';
+        }
     }
 
     public function ReadByID(int $id): ?object {
